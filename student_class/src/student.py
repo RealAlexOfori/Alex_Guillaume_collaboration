3d4e53695e5fba73a4f09cf97ed3e03ecfdc617f
user <user@users-MacBook-Pro.local>
2023-03-06 15:51:24 +0000

    Completed part a of lab

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,1 +1,12 @@
+class Student:
+    def __init__(self, name, cohort):
+        self.name = str(name)
+        self.cohort = str(cohort)
+
+    def talk(self):
+        return "I can talk!"  
+
+    def say_favourite_language(self,language):
+        return "I love " + language     
+
 
